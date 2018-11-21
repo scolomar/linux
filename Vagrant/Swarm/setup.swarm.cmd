0bb6d4c07ab1320207edb9faf1d7972d21dbc626
secobau <sebastian.colomar@gmail.com>
2018-11-21 23:17:09 +0100

    Update setup.swarm.cmd

--- Vagrant/Swarm/setup.swarm.cmd
+++ Vagrant/Swarm/setup.swarm.cmd
@@ -8,4 +8,4 @@ FOR %%x IN (M1,Mx,Wx) DO (
 cd M1
 vagrant ssh M1 -c "for x in $(sudo docker node ls -q -f name=M);do sudo docker node update --availability drain $x;done"
 vagrant ssh M1 -c "curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/stack.yml -O"
-vagrant ssh M1 -c "sudo docker stack deploy -c stack.yml MyStack"
+vagrant ssh M1 -c "sudo docker stack deploy -c stack.yml my"
