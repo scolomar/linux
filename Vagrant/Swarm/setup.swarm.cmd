2746dc317ee23567027601f6e027539f3536296d
secobau <sebastian.colomar@gmail.com>
2018-11-21 23:15:20 +0100

    Update setup.swarm.cmd

--- Vagrant/Swarm/setup.swarm.cmd
+++ Vagrant/Swarm/setup.swarm.cmd
@@ -3,7 +3,7 @@ FOR %%x IN (M1,Mx,Wx) DO (
  mkdir %%x
  cd %%x
  curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/%%x/Vagrantfile -O
- vagrant up
+ vagrant up --parallel
  cd %d% )
 cd M1
 vagrant ssh M1 -c "for x in $(sudo docker node ls -q -f name=M);do sudo docker node update --availability drain $x;done"
