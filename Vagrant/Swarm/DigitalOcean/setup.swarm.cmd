880bb4ef6f2fa9ef96ef106ac4c0e305696ccf11
secobau <sebastian.colomar@gmail.com>
2018-11-27 18:48:55 +0100

    Update setup.swarm.cmd

--- Vagrant/Swarm/DigitalOcean/setup.swarm.cmd
+++ Vagrant/Swarm/DigitalOcean/setup.swarm.cmd
@@ -1,11 +1,10 @@
 vagrant plugin install vagrant-digitalocean
-SET d="C:\Users\usuario\Desktop\Vagrant\DigitalOcean\Swarm"
 FOR %%x IN (M1,Mx,Wx) DO (
  mkdir %%x
  cd %%x
  curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/DigitalOcean/%%x/Vagrantfile -O
  vagrant up --parallel
- cd %d% )
+ cd .. )
 cd M1
 vagrant ssh M1 -c "for x in $(sudo docker node ls -q -f name=M);do sudo docker node update --availability drain $x;done"
 vagrant ssh M1 -c "curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/DigitalOcean/stack.yml -O"
