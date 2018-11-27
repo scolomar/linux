073efd9d1d4147ec417295f820d397a4fdd39ef5
secobau <sebastian.colomar@gmail.com>
2018-11-27 18:49:23 +0100

    Update setup.swarm2.cmd

--- Vagrant/Swarm/DigitalOcean/setup.swarm2.cmd
+++ Vagrant/Swarm/DigitalOcean/setup.swarm2.cmd
@@ -1,8 +1,7 @@
 vagrant plugin install vagrant-digitalocean
-SET d="C:\Users\usuario\Desktop\Vagrant\DigitalOcean\Swarm"
 FOR %%x IN (Wx2) DO (
  mkdir %%x
  cd %%x
  curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/DigitalOcean/%%x/Vagrantfile -O
  vagrant up --parallel
- cd %d% )
+ cd .. )
