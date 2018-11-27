058dea712c4e43ac1708d874ad231a6d92720b8d
secobau <sebastian.colomar@gmail.com>
2018-11-27 20:28:59 +0100

    Update setup.swarm2.cmd

--- Vagrant/Swarm/DigitalOcean/setup.swarm2.cmd
+++ Vagrant/Swarm/DigitalOcean/setup.swarm2.cmd
@@ -1,7 +1,18 @@
 vagrant plugin install vagrant-digitalocean
+SETLOCAL EnableDelayedExpansion
+FOR /F %%z IN (token) DO (
+ SET z=%%x
+ )
 FOR %%x IN (Wx2) DO (
  mkdir %%x
  cd %%x
  curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/DigitalOcean/%%x/Vagrantfile -O
+ FOR /F "tokens=* delims= " %%x IN (Vagrantfile) DO (
+  SET y=%%x
+  SET y=!y:xxx=%z%!
+  ECHO !y!>>Vagrantfile.NEW
+  MOVE Vagrantfile.NEW Vagrantfile
+  )
  vagrant up --parallel
- cd .. )
+ cd ..
+ )
