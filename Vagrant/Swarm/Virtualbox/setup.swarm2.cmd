0548306e63207352bb38110468f0e139b755ce83
secobau <sebastian.colomar@gmail.com>
2018-11-29 14:40:09 +0100

    Update setup.swarm2.cmd

--- Vagrant/Swarm/Virtualbox/setup.swarm2.cmd
+++ Vagrant/Swarm/Virtualbox/setup.swarm2.cmd
@@ -4,3 +4,6 @@ FOR %%x IN (Wx2) DO (
  curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/Virtualbox/%%x/Vagrantfile -O
  vagrant up --parallel
  cd .. )
+cd M1
+vagrant ssh M1 -c "curl https://raw.githubusercontent.com/secobau/linux/master/Vagrant/Swarm/Virtualbox/stack2.yml -O";
+vagrant ssh M1 -c "sudo docker stack deploy -c stack2.yml my";
